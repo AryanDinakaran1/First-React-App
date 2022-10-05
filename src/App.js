9994cb232f9314aff5d645c841d36d6a2e25e733
Aryan Dinakaran <arydinakarangmail.com@Aryans-MacBook-Pro.local>
2022-10-05 19:31:33 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello, World!</p>
+
+      <Info></Info>
     </div>
   );
+}
+
+function Info() {
+
+  const title = "We are cool!"
+  return (
+    <div>
+      <h1>Inventory System</h1>
+      <p>{ title }</p>
+    </div>
+  )
 }
 
 export default App;
